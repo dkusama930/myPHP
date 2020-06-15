194c74e9c0257b55c78678c9a3d660dbca9859a9
d.kusama <d.kusama@high-wizard.com>
2020-06-15 15:09:27 +0900

    [update] add index.html for HTTP test

--- echoPax.php
+++ echoPax.php
@@ -1,2 +1,9 @@
 <?php
-echo 'Pax!';
+
+if (isset($_GET['hoge'])){
+    echo $_GET['hoge'];
+} else if (isset($_POST['fuga'])){
+    echo $_POST['fuga'];
+} else {
+    echo 'can not get a price with either get or post';
+}
